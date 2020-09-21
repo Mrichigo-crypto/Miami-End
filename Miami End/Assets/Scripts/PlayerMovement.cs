8d429c20f0cd5fc2b86d7624a91f51e0890e621b
Muntasir Chowdhury Mridul <54744758+Mrichigo-crypto@users.noreply.github.com>
2020-09-22 03:11:52 +0600

    Movement Bug Fixed

--- Miami End/Assets/Scripts/PlayerMovement.cs
+++ Miami End/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,8 @@ public class PlayerMovement : MonoBehaviour
   private float _inputX;
   private float _inputZ;
   private float _verticalVel;
-  
-  private Vector3 _moveDirection;
+
   private Vector3 _moveVector;
-  private bool _bloackRotation = false;
-  private bool _isGrounded;
   private Animator _anim;
   private CharacterController _controller;
 
@@ -35,70 +32,49 @@ public class PlayerMovement : MonoBehaviour
 
    void Update()
    {
+     _inputX = Input.GetAxis("Horizontal");
+     _inputZ = Input.GetAxis("Vertical");
+ 
      
      InputMagnitude();
-     Move();
+     RegularMovement();
+    
       
    }
-
-  
-    void Move()
+    
+   void RegularMovement()
    {
-     _moveVector = new Vector3(_inputX,0, _inputZ).normalized;
-      Vector3 velocity = _moveVector * _moveSpeed;
+      _moveVector = new Vector3(_inputX , 0 , _inputZ);
+      _moveVector = _mainCam.transform.TransformDirection(_moveVector);
 
-     _isGrounded = _controller.isGrounded;
-     if(_isGrounded)
-    {
-      if(Input.GetKeyDown(KeyCode.Space))
+      if((_inputX > 0.1f || _inputZ > 0.1f) || (_inputX < -0.1f || _inputZ < -0.1f))
       {
+         var rotation = Quaternion.LookRotation(new Vector3(_moveVector.x , 0 , _moveVector.z));
+         transform.rotation = Quaternion.Slerp(transform.rotation , rotation , Time.deltaTime * _rotationSpeed);
+      }
+
+      //Jump       
+        
+     if(_controller.isGrounded && Input.GetKeyDown(KeyCode.Space) )
+     {
          _anim.SetTrigger("Jump");
          _verticalVel = _jumpForce;
-         
-
-      }
-      else
-       {
-          
-         _verticalVel -= _gravity;
-         
-       }
+              
     }
     else
      {
-      _verticalVel -= _gravity;
+       _verticalVel -= _gravity;
      }
-     
-      velocity.y = _verticalVel;
-   
-      _controller.Move(velocity * Time.deltaTime);
-   }
-   void PlayerMoveCalc()
-    {
-       _inputX = Input.GetAxis("Horizontal");
-       _inputZ = Input.GetAxis("Vertical");
- 
-         var forward = _mainCam.transform.forward;
-         var right = _mainCam.transform.right;
-         forward.y = 0f;
-         right.y = 0f;
-         forward.Normalize();
-         right.Normalize();
-         
-         _moveDirection = forward * _inputZ + right * _inputX;
-         
-         if(_bloackRotation == false)
-         {
-          transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_moveDirection),_rotationSpeed);
-         }
-    
-    }
 
+      Vector3 velocity = _moveVector * _moveSpeed;
+      velocity.y = _verticalVel;
+
+     _controller.Move(velocity * Time.deltaTime);
+     
+   }
    void InputMagnitude()
    {
-      _inputX = Input.GetAxis("Horizontal");
-      _inputZ = Input.GetAxis("Vertical");
- 
+      
      _anim.SetFloat("InputZ",_inputZ,0.0f,Time.deltaTime * 2f);
      _anim.SetFloat("InputX",_inputX,0.0f,Time.deltaTime * 2f);
 
@@ -107,7 +83,6 @@ public class PlayerMovement : MonoBehaviour
      if(_speed > _allowPlayerRotation)
        {
         _anim.SetFloat("InputMagnitude", _speed, 0.0f , Time.deltaTime);
-        PlayerMoveCalc();
        }
      else if(_speed < _allowPlayerRotation)
        {
